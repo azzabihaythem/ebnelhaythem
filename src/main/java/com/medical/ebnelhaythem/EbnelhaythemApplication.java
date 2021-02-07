6c10261da3e881084d28120f366f07358a4b6ee4
Haythem Azzabi <azzabi.haythem@gmail.com>
2021-02-07 18:23:16 +0100

    first commit - create spring boot project

---
+++ src/main/java/com/medical/ebnelhaythem/EbnelhaythemApplication.java
@@ -0,0 +1,13 @@
+package com.medical.ebnelhaythem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbnelhaythemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbnelhaythemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/ebnelhaythem/EbnelhaythemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.ebnelhaythem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbnelhaythemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
