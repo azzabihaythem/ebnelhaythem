6123c853f9cdb82ad76e0ae68d8ae43a3b8e168a
Haythem Azzabi <azzabi.haythem@gmail.com>
2021-02-28 10:36:27 +0100

    create banner

--- src/main/java/com/medical/ebnelhaythem/EbnelhaythemApplication.java
+++ src/main/java/com/medical/ebnelhaythem/EbnelhaythemApplication.java
@@ -1,12 +1,38 @@
 package com.medical.ebnelhaythem;
 
+import com.medical.ebnelhaythem.entity.Role;
+import com.medical.ebnelhaythem.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class EbnelhaythemApplication {
 
-	public static void main(String[] args) {
+	private static Logger logger = Logger.getLogger(String.valueOf(EbnelhaythemApplication.class));
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@PostConstruct
+	public void init() {
+
+		List<Role> roleList = roleRepository.findAll();
+		logger.info("init *****************");
+		if(roleList.isEmpty()){
+			roleList.add(new Role("admin"));
+			roleList.add(new Role("patient"));
+			roleRepository.saveAll(roleList);
+		}
+	}
+
+
+
+		public static void main(String[] args) {
 		SpringApplication.run(EbnelhaythemApplication.class, args);
 	}
 
