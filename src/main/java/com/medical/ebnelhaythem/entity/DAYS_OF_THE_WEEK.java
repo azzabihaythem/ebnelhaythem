1877e519567b89baf8a36b288063e9fae028e2bf
Haythem Azzabi <azzabi.haythem@gmail.com>
2023-02-13 00:26:53 +0100

    add seance days

---
+++ src/main/java/com/medical/ebnelhaythem/entity/DAYS_OF_THE_WEEK.java
@@ -0,0 +1,14 @@
+package com.medical.ebnelhaythem.entity;
+
+import javax.persistence.Table;
+
+@Table
+public enum DAYS_OF_THE_WEEK {
+    SUN,
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT;
+}
